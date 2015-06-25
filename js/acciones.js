ddc20269b839dc67d31a0e40f9df983700adfd48
gaby <gaby_morales626@live.com>
2015-06-25 11:36:19 -0500

    uno

---
+++ js/acciones.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+$('#btnanimales').on ('tap', function() {
+	navigator.vibrate(10000);
+	$('#emperor').on ('tap', function() {
+	navigator.vibrate(10000);
+	$('#mixino').on ('tap', function() {
+	navigator.vibrate(10000);
+	$('#ajolote').on ('tap', function() {
+	navigator.vibrate(10000);
+	$('#tarsier').on ('tap', function() {
+	navigator.vibrate(10000);
+	$('#dumbo').on ('tap', function() {
+	navigator.vibrate(10000);
+	$('#inicio').on ('tap', function() {
+	navigator.vibrate(10000);
+});
+});
+});
+});
+});
+});
+});
+}); 
+});
+
+
+
+
