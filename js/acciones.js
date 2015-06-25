e6b00963611011117bb3f3d033bd1ab60cd01c21
gaby <gaby_morales626@live.com>
2015-06-25 11:57:25 -0500

    tres

--- js/acciones.js
+++ js/acciones.js
@@ -2,19 +2,19 @@
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 $('#btnanimales').on ('tap', function() {
-	navigator.vibrate(10000);
+	navigator.vobrate(20000);
 	$('#emperor').on ('tap', function() {
-	navigator.vibrate(10000);
+	navigator.vobrate(20000);
 	$('#mixino').on ('tap', function() {
-	navigator.vibrate(10000);
+	navigator.vobrate(20000);
 	$('#ajolote').on ('tap', function() {
-	navigator.vibrate(10000);
+	navigator.vobrate(20000);
 	$('#tarsier').on ('tap', function() {
-	navigator.vibrate(10000);
+	navigator.vobrate(20000);
 	$('#dumbo').on ('tap', function() {
-	navigator.vibrate(10000);
+	navigator.vobrate(20000);
 	$('#inicio').on ('tap', function() {
-	navigator.vibrate(10000);
+	navigator.vobrate(20000);
 });
 });
 });
